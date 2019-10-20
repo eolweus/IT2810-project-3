36c63229b41b67541f21f2079354ffe3cf292fa5
kristhaj <kristhaj@stud.ntnu.no>
2019-10-20 15:24:55 +0200

    add simple implementation of filtering #9

--- app/src/App.js
+++ app/src/App.js
@@ -12,6 +12,9 @@ class App extends Component{
     //empty input field in order to enable adding more songs
     this.song.value = "";
   }
+  filter = (e) => {
+    this.props.SongStore.filter = e.target.value;
+  }
 
   render() {
     const {SongStore} = this.props;
@@ -21,13 +24,16 @@ class App extends Component{
 
           <h2>You have {SongStore.songCount} songs in your playlist.</h2>
 
+          <div>{SongStore.filter}</div>
+          <input className="filter" value={SongStore.filter} onChange={this.filter.bind(this)}/>
+
           <form onSubmit={e => this.handleSubmit(e)}>
             <input type="text" placeholder="Enter song title" ref={input => this.song = input}/>
             <button>Add Song</button>
           </form>
 
           <ul>
-            {SongStore.songs.map(song => (
+            {SongStore.filteredSongs.map(song => (
                 <li key={song}>
                   {song}
                 </li>
