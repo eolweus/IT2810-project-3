2195bd8ac8d5f783e3ed06cb75e4ba7c25366ccc
eolweus <37080270+eolweus@users.noreply.github.com>
2019-10-22 13:32:10 +0200

    Adds a basic template for express in server.js

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.get('', (req, res) => {
+    // her må det fylles inn kobling til databasen
+
+    // Her må det fylles inn noe mer
+    res.json()
+})
+
+
+// Denne må endres. Bruker 5000 midlertidig for å ha noe å kjøre lokalt
+const port = 5000
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
