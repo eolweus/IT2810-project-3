9cf086af9330a5647a66e0f0e4279c1703ff870d
Sander Hestvik <sander.hestvik@gmail.com>
2019-10-22 12:45:40 +0200

    Set correct ID to tracks in DB #4

--- database/ElasticSearchFeeder.js
+++ database/ElasticSearchFeeder.js
@@ -6,7 +6,7 @@ async function feed() {
     let tracks = fs.readFileSync('tools/tracks.json', 'utf8');
     let jsonData = JSON.parse(tracks);
     //TODO map spotify ID slik at den blir dokoment ID
-    const body = jsonData.flatMap((track => [{index: {_index:'tracks'}}, track]));
+    const body = jsonData.flatMap((track => [{index: {_index:'tracks', "_id":track.id}}, track]));
     const {body: bulkresponse} = await client.bulk({refresh: true, body})
 
 
