9eba52bca849fa7f07eb12e922e6b8b57b1382d1
Sander Hestvik <sander.hestvik@gmail.com>
2019-10-23 14:02:11 +0200

    Add sorting example #4

--- database/ExampleQuery.js
+++ database/ExampleQuery.js
@@ -1,11 +1,11 @@
 const {Client} = require('@elastic/elasticsearch');
 const client = new Client({node: 'http://localhost:9200'});
 
-function searchExample(userInput, pageSize, fromElement){
+function searchExample(userInput, pageSize, fromElement) {
     return client.search({
         index: 'tracks',
-        from:   fromElement, //From and size is for pagination
-        size : pageSize,
+        from: fromElement, //From and size is for pagination
+        size: pageSize,
         body: {
             "query": {
                 "bool": {
@@ -24,5 +24,45 @@ function searchExample(userInput, pageSize, fromElement){
         }
     });
 }
+/*
+    *@Param {String} userInput - string with the search input
+    *@Param {String} sortBy - string describing what to sort some valid inputs are 'popularity', 'album.total_tracks'
+    *,'album.release_date', 'duration_ms' */
+function searchExampleWithSorting(userInput, pageSize, fromElement, sortBy, sortAsc) {
+    let searchObject = {
+        index: 'tracks',
+        from: fromElement, //From and size is for pagination
+        size: pageSize,
+        body: {
+            'sort': {},
+            "query": {
+                "bool": {
+                    "must": {
+                        "multi_match": {
+                            "query": userInput,
+                            "fields": [//Fields that are searched
+                                "name",
+                                "artists.name",
+                                "album.name"
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+    };
+    searchObject.body.sort[sortBy] = {'order': (sortAsc ? 'asc' : 'desc')};
+    return client.search(searchObject);
+}
 
-searchExample("this is america", 10, 0).then((result) => {console.log(result.body.hits.hits)});
+/*searchExample("this is america", 10, 0).then((result) => {
+    console.log(result.body.hits.hits)
+});*/
+searchExampleWithSorting("this is america", 10, 0, 'duration_ms', true).then((result) => {
+    console.log(result.body.hits.hits)
+});
+/*
+searchExampleWithSorting("this is america", 40, 0, 'album.release_date', false).then((result) => {
+    result.body.hits.hits.forEach((track) => console.log(track._source.album))
+});
+*/
