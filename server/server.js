c18957cb060e39254cac957ddae029ad0d36ca26
eolweus <37080270+eolweus@users.noreply.github.com>
2019-10-24 15:42:23 +0200

    Add get functionality to API #10
    Co-Authored-By: Sander Hestvik <sander.hestvik@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,16 +1,38 @@
+const Queries = require('./queries.js')
+const queries = new Queries()
 const express = require('express')
-const {Client} = require('@elastic/elasticsearch');
-const client = new Client({node: 'http://localhost:9200'});
+//const {Client} = require('@elastic/elasticsearch');
+//const client = new Client({node: 'http://localhost:9200'});
 
+console.log(queries);
 const app = express()
 
-app.get('', (req, res) => {
-    // her må det fylles inn kobling til databasen
+app.get('/api/tracks', (req, res) => {
+    const searchString = req.query.searchString;
+    const limit = (req.query.limit === undefined ? 20 : req.query.limit);
+    const offset = (req.query.offset === undefined ? 0 : req.query.offset);
+    const sortBy = req.query.sortBy;
+    let sortOrder = req.query.sortOrder;
 
-    // Her må det fylles inn noe mer
-    res.json()
+    if (sortBy === undefined || sortOrder === undefined) {
+        queries.search(searchString, limit, offset).then((result)=>res.json(result)).catch((error) => {console.log(error);
+        res.send(error)});
+    }
+    else if(!['asc', 'desc'].includes(sortOrder)){
+       res.send("sortOrder has to be asc, desc or unused");
+    }
+    else {
+      /*
+          *Function searchWithSorting
+          *@Param {String} userInput - string with the search input
+          *@Param {String} sortBy - string describing what to sort some valid inputs are 'popularity', 'album.total_tracks'
+          *,'album.release_date', 'duration_ms', 'name.keyword', 'album.name.keyword' */
+        sortOrder = (sortOrder === 'asc' ? true : false); // false means desc
+        queries.searchWithSorting(searchString, limit, offset, sortBy, sortOrder).then((result)=>res.json(result)).catch((error) => {console.log(error)
+        res.send(error)});
+    }
+    console.dir(req.query);
 })
-
 
 // Denne må endres. Bruker 5000 midlertidig for å ha noe å kjøre lokalt
 const port = 5000
