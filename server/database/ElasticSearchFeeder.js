a0eaa57255807ab2c0d240039f71d1d06289e54e
Sander Hestvik <sander.hestvik@gmail.com>
2019-10-25 15:23:10 +0200

    Make ElasticSearchFeeder wipe database before it feeds #14

--- server/database/ElasticSearchFeeder.js
+++ server/database/ElasticSearchFeeder.js
@@ -3,14 +3,21 @@ const {Client} = require('@elastic/elasticsearch');
 const client = new Client({node: 'http://localhost:9200'});
 
 async function feed() {
+    await client.indices.delete({
+        index: 'tracks',
+    }).then(function (res) {
+            console.log("Successful delete!");
+            console.log(JSON.stringify(res, null, 4));
+        }
+    );
     let tracks = fs.readFileSync('tools/tracks.json', 'utf8');
     let jsonData = JSON.parse(tracks);
     const body = jsonData.flatMap((track) => {
         track['total_user_reviews'] = 0;
         track['cumulated_user_review_score'] = 0;
-        return [{index: {_index:'tracks', '_id':track.id}}, track];
-});
-    const {body: bulkresponse} = await client.bulk({refresh: true, body})
+        return [{index: {_index: 'tracks', '_id': track.id}}, track];
+    });
+    const {body: bulkresponse} = await client.bulk({refresh: true, body}).then("Successfully added tracks");
 
 
 }
