1a9ebbd4951cb8cc3fad45f5040d403f8b128f29
kristhaj <kristhaj@stud.ntnu.no>
2019-10-26 14:26:13 +0200

    add store for query filters #19

---
+++ app/src/stores/QueryStore.js
@@ -0,0 +1,48 @@
+import { observable, action } from "mobx";
+
+class QueryStore {
+    @observable searchString;
+    @observable limit;
+    @observable offset;
+    @observable filterBy;
+    @observable greaterThan;
+    @observable sortBy;
+    @observable sortOrder;
+
+    @action setSearchString = (string) => {
+        this.searchString = string;
+    }
+
+    @action setLimit = (limit) => {
+        this.limit = limit;
+    }
+    @action setOffset = (offset) => {
+        this.offset = offset;
+    }
+    @action setFilter = (filterBy, greaterThan) => {
+        this.filterBy = filterBy;
+        this.greaterThan = greaterThan;
+    }
+
+    @action setSort = (sortBy, sortOrder) => {
+        this.sortBy = sortBy;
+        this.sortOrder = sortOrder;
+    }
+
+    @computed getQueryString() {
+        return [this.searchString, this.limit, this.offset, this.filterBy, this.greaterThan, this.sortBy, this.sortOrder];
+    }
+
+    @action clearFilter = () => {
+        this.setSearchString(null)
+        this.setLimit(null);
+        this.setOffset(null);
+        this.setFilter(null, null);
+        this.setSort(null, null);
+    }
+
+
+}
+
+const store = new QueryStore();
+export default store;
